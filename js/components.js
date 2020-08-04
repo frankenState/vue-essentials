79fa3bbce0278447a937053be4140d2d9bf348df
Frank Lou <frank@nxt.work>
2020-08-05 00:37:02 +0800

    Saving last state.

--- js/components.js
+++ js/components.js
@@ -4,10 +4,25 @@ Vue.component('AvatarComponent', {
 	<div class="media">
 	  <img :src="user.avatar" class="mr-3" width="60rem">
 	  <div class="media-body text-justify">
-	    <h5 class="mt-0 mb-0">{{ user.name }}</h5>
+	    <h5 class="mt-0 mb-0">{{ user.name }}
+	    	<div class="btn-group btn-sm float-right" role="group" aria-label="Basic example">
+		  		<button type="button" class="btn btn-outline-info btn-sm">Edit</button>
+		  		<button type="button" class="btn btn-outline-danger btn-sm">Delete</button>
+			</div>
+	    </h5>
 	    {{ user.details }}
 	  </div>
 	</div>
-
 	`
 });
+
+
+
+
+
+
+
+
+
+
+
